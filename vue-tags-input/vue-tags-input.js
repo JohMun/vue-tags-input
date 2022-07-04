9772045482fb74c3d2c80378695b38b793945f90
Roman Mavliutov <mavlutovr@ya.ru>
2022-07-04 19:59:06 +0300

    fix: open on focus

--- vue-tags-input/vue-tags-input.js
+++ vue-tags-input/vue-tags-input.js
@@ -379,11 +379,8 @@ export default {
       }
 
       setTimeout(() => {
-        console.log('open', 1)
         this.focused = true;
-        console.log(2)
         focus && setTimeout(() => {
-          console.log(3)
           this.focus();
         });
       })
